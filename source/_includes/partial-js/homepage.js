9613c2f1a756e25af34508f5471d706fd521c377
Brewhouse Digital <zach@brewhouse.digital>
2021-03-21 11:12:28 -0500

    Improved the mobile experience

--- source/_includes/partial-js/homepage.js
+++ source/_includes/partial-js/homepage.js
@@ -5,5 +5,19 @@ new Splide('#homepage-slider', {
 		left : '160px',
 	},
 	updateOnMove: true,
-	speed: 800
+	speed: 800,
+	breakpoints: {
+		1140: {
+			padding: {
+				right: '100px',
+				left : '100px',
+			}
+		},
+		768: {
+			padding: {
+				right: '40px',
+				left : '40px',
+			}
+		}
+	}
 }).mount();
